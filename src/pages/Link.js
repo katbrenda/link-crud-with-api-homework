aabd0cd1a0346c93c8cb3b495305ed457af16684
Katherine <katbrenda1988@gmail.com>
2022-03-01 23:05:53 -0700

    added to homework

--- src/pages/Link.js
+++ src/pages/Link.js
@@ -1,11 +1,17 @@
-import React from "react";
+// import React from "react";
+import React, {useContext} from 'react'
+import { DataContext } from '../providers/DataProvider'
+
 const Link = (props) =>{
+    const { links, getLinks, createLink, updateLink ,deleteLink} = useContext(DataContext)
     return(
         <div>
             <h1>Id: {props.id}</h1>
             <h1>Username: {props.username}</h1>
             <h1>Title: {props.title}</h1>
-//maybe add buttons
+            <button onClick={()=>deleteLink(props.id)}>delete link</button>
+            <button onClick={()=>updateLink(props.id)}>edit link</button>
+{/* maybe add buttons */}
         </div>
     )
 }
--- src/pages/Links.js
+++ src/pages/Links.js
@@ -1,14 +1,27 @@
 import React, {useContext} from 'react'
 import { DataContext } from '../providers/DataProvider'
 import Link from './Link'
+import { Link as ReactRouterLink } from "react-router-dom";
 
 const Links = ()=>{
     //useContext always return object just destructor keys
     
     const { links, getLinks, createLink, updateLink ,deleteLink} = useContext(DataContext)
     const renderLinks= ()=>{
-        return links.map((link)=>{
-            return <Link key={link.id} {...link} deleteLink={deleteLink} updateLink={updateLink}/>
+        return links.map((props)=>{
+            return(
+                <div>
+                    <h1>Id: {props.id}</h1>
+                    <h1>Username: {props.username}</h1>
+                    <h1>Title: {props.title}</h1>
+                    <button onClick={()=>deleteLink(props.id)}>delete link</button>
+                    <button onClick={()=>updateLink(props.id)}>update link</button>
+                    <ReactRouterLink to={`links/${props.id}/edit`} state={{...props}}>edit</ReactRouterLink>
+        {/* maybe add buttons */}
+                </div>
+            )
+            // return <Link key={link.id} {...link} deleteLink={deleteLink} updateLink={updateLink}/>
+
         })
     }
     
@@ -23,6 +36,7 @@ const Links = ()=>{
         <button onClick={()=>createLink( {title:'title here', username:'katherine'} )}>create link</button>
         <button onClick={()=>updateLink( {id:19, title:'UPDATED'} )}>update link</button>
         <button onClick={()=>deleteLink(19)}>delete link</button>
+
         </div>
     )
 }
